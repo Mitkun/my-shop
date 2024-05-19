fa69120311686e4f46af47bb408621383e6a7bbf
thientruong <truongthien@aiontech.vn>
2024-05-19 17:55:11 +0700

    update file sql

--- data.sql
+++ data.sql
@@ -1,32 +1,47 @@
 CREATE TABLE `categories` (
-    `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `image` json DEFAULT NULL,
-    `decsription` text,
-    `status` enum('activated','deactivated') DEFAULT 'activated',
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                              `id` varchar(36) NOT NULL,
+                              `title` varchar(250) NOT NULL,
+                              `status` enum('activated','deactivated') DEFAULT 'activated',
+                              `thumbnail` varchar(500) DEFAULT NULL,
+                              `created_by` varchar(36) DEFAULT NULL,
+                              `updated_by` varchar(36) DEFAULT NULL,
+                              `deleted_by` varchar(36) DEFAULT NULL,
+                              `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                              `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                              `deleted_at` timestamp NULL DEFAULT NULL,
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `products` (
-  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `category_id` varchar(36) DEFAULT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `image` json DEFAULT NULL,
-  `type` enum('drink','food','topping') NOT NULL DEFAULT 'drink',
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `status` enum('activated','deactivated','out_of_stock') DEFAULT 'activated',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                            `id` varchar(36) NOT NULL,
+                            `category_id` int NOT NULL,
+                            `name` varchar(250) NOT NULL,
+                            `qty` int NOT NULL,
+                            `cost_of_capital` float NOT NULL,
+                            `sale_qty` int NOT NULL,
+                            `type_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                            `price` float NOT NULL,
+                            `post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                            `discount` int DEFAULT NULL,
+                            `status` enum('activated','deactivated','out_of_stock','coming_soon') DEFAULT 'activated',
+                            `thumbnail` varchar(500) DEFAULT NULL,
+                            `created_by` varchar(36) DEFAULT NULL,
+                            `updated_by` varchar(36) DEFAULT NULL,
+                            `deleted_by` varchar(36) DEFAULT NULL,
+                            `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                            `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                            `deleted_at` timestamp NULL DEFAULT NULL,
+                            PRIMARY KEY (`id`),
+                            KEY `category_id` (`category_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
 
 CREATE TABLE `users` (
                          `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-                         `first_name` varchar(50) DEFAULT NULL,
-                         `last_name` varchar(50) DEFAULT NULL,
+                         `first_name` varchar(150) DEFAULT NULL,
+                         `last_name` varchar(150) DEFAULT NULL,
+                         `address` varchar(150) DEFAULT NULL,
+                         `phone_number` varchar(50) DEFAULT NULL,
                          `email` varchar(100) DEFAULT NULL,
                          `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
                          `salt` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
@@ -37,7 +52,7 @@ CREATE TABLE `users` (
                          `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                          PRIMARY KEY (`id`),
                          UNIQUE KEY `email` (`email`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `user_sessions` (
                                  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -49,8 +64,9 @@ CREATE TABLE `user_sessions` (
                                  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                                  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                  PRIMARY KEY (`id`),
-                                 KEY `user_id` (`user_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                                 KEY `user_id` (`user_id`) USING BTREE,
+                                 KEY `refresh_token` (`refresh_token`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `images` (
                           `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
